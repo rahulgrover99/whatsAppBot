f409f37cd75b9b4b4bea0d91ea6789af3a672e55
Avi Wadhwa <aviwad@gmail.com>
2019-06-11 17:11:07 +0530

    works

--- mainBot.py
+++ mainBot.py
@@ -12,9 +12,7 @@ import time
 import openpyxl as excel
 import os
 
-msgToSend = [
-                [12, 32, 0, "Hello! This is test Msg. Please Ignore." + Keys.SHIFT + Keys.ENTER + "http://bit.ly/mogjm05"]
-            ]
+msgToSend = [12, 32, 0, "ur mum gae", "https://www.youtube.com/watch?v=dQw4w9WgXcQ"]
 # Driver to open a browser
 options = webdriver.ChromeOptions()
 options.add_argument(argument="--user-data-dir="+os.path.expanduser("~")+"/.config/google-chrome")
@@ -28,57 +26,56 @@ driver.get("https://web.whatsapp.com/")
 # 10 sec wait time to load, if good internet connection is not good then increase the time
 # units in seconds
 # note this time is being used below also
+wait = WebDriverWait(driver, 10)
 wait5 = WebDriverWait(driver, 5)
-target = "Adyan Monty"
+target = "Chai Knees"
+#TODO make based on target
+x_arg = "//span[contains(@title,'"+target+"')]"
 
-try:
-    # Select the target
-    x_arg = '//span[contains(@title,' + target + ')]'
-    try:
-        wait5.until(EC.presence_of_element_located((
-            By.XPATH, x_arg
-        )))
-    except:
-        print("FIRST LOL")
-        # If contact not found, then search for it
-        searBoxPath = '//*[@id="input-chatlist-search"]'
-        wait5.until(EC.presence_of_element_located((
-            By.ID, "input-chatlist-search"
-        )))
-        inputSearchBox = driver.find_element_by_id("input-chatlist-search")
-        time.sleep(0.5)
-        # click the search button
-        driver.find_element_by_xpath('/html/body/div/div/div/div[2]/div/div[2]/div/button').click()
-        time.sleep(1)
-        inputSearchBox.clear()
-        inputSearchBox.send_keys(target[1:len(target) - 1])
-        print('Target Searched')
-        # Increase the time if searching a contact is taking a long time
-        time.sleep(4)
+#wait5.until(EC.presence_of_element_located((
+#    By.XPATH, x_arg
+#)))
+#try:
+# Select the target
+#x_arg = '//span[contains(@title,' + target + ')]'
+# If contact not found, then search for it
+searBoxPath = '//*[@id="input-chatlist-search"]'
 
-    # Select the target
-    driver.find_element_by_xpath(x_arg).click()
-    print("Target Successfully Selected")
-    time.sleep(2)
+time.sleep(0.5)
+# click the search button
+time.sleep(5)
+driver.find_element_by_xpath("//button[contains(@class,'_1XCAr')]").click()
+inputSearchBox = driver.find_element_by_xpath("//input[contains(@title,'Search or start new chat')]")
+inputSearchBox.send_keys(target)
+print('Target Searched')
+# Increase the time if searching a contact is taking a long time
+time.sleep(5)
 
-    # Select the Input Box
-    inp_xpath = "//div[@contenteditable='true']"
-    input_box = wait.until(EC.presence_of_element_located((
-        By.XPATH, inp_xpath)))
-    time.sleep(1)
+driver.find_element_by_xpath(x_arg).click()
+print("Target Successfully Selected")
+time.sleep(2)
 
-    # Send message
-    # taeget is your target Name and msgToSend is you message
-    input_box.send_keys("Hello, " + target + "."+ Keys.SHIFT + Keys.ENTER + msgToSend[count][3] + Keys.SPACE) # + Keys.ENTER (Uncomment it if your msg doesnt contain '\n')
-    # Link Preview Time, Reduce this time, if internet connection is Good
-    time.sleep(10)
+# Select the Input Box
+inp_xpath = "//div[@contenteditable='true']"
+input_box = wait.until(EC.presence_of_element_located((
+    By.XPATH, inp_xpath)))
+time.sleep(1)
+
+# Send message
+# taeget is your target Name and msgToSend is you message
+for i in range(0,2):
+    input_box.send_keys("Hello, " + target + "."+ Keys.SHIFT + Keys.ENTER + msgToSend[3])
     input_box.send_keys(Keys.ENTER)
-    print("Successfully Send Message to : "+ target + '\n')
-    success+=1
-    time.sleep(0.5)
+    input_box.send_keys(msgToSend[4])
+    input_box.send_keys(Keys.ENTER)  # + Keys.ENTER (Uncomment it if your msg doesnt contain '\n')
+# Link Preview Time, Reduce this time, if internet connection is Good
+time.sleep(10)
+print("Successfully Send Message to : "+ target + '\n')
+success+=1
+time.sleep(0.5)
 
-except:
-    print("LOL")
+#except:
+#    print("LOL")
 
 #inp_xpath = "//div[@contenteditable='true']"
 #input_box = wait.until(EC.presence_of_element_located((
