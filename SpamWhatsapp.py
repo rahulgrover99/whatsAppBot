b30e125c365f29a2198057fff63d13abf629a9e5
Avi Wadhwa <aviwad@gmail.com>
2019-06-13 15:36:30 +0530

    kiosk, check and show screenshot for qr

--- SpamWhatsapp.py
+++ SpamWhatsapp.py
@@ -13,6 +13,9 @@ from flask import (
 app = Flask(__name__)
 
 options = webdriver.ChromeOptions()
+options.add_argument(argument="--kiosk")
+options.add_argument(argument='--window-size=1366x768');
+options.add_argument(argument="--start-maximized")
 options.add_argument(argument="--user-data-dir="+os.path.expanduser("~")+"/.config/google-chrome")
 executable_path = os.path.expanduser("~")+"/Projects/whatsAppBot/chromedriver_linux64/chromedriver"
 driver = webdriver.Chrome(executable_path=executable_path, options=options)
@@ -45,13 +48,16 @@ def send_whatsapp_message(data):
 
     try:
         driver.find_element_by_xpath(x_arg).click()
+        print('number clicked')
     except:
         driver.find_element_by_xpath(x_arg_name).click()
+        print('group clicked')
 
     print("Target Successfully Selected")
     time.sleep(2)
 
     # Select the input box
+    '''
     inp_xpath = "//div[@contenteditable='true']"
     input_box = wait.until(EC.presence_of_element_located((
         By.XPATH, inp_xpath)))
@@ -62,8 +68,21 @@ def send_whatsapp_message(data):
     for i in range(0,int(data["loopnumber"])):
         input_box.send_keys(data["message"])
         input_box.send_keys(Keys.ENTER)
+    '''
 
+time=time.time()
 #send_whatsapp_message(data)
+@app.route("/qr")
+def qrcode():
+    save_screenshot()
+    return render_template("screenshot.html",time=time)
+
+def save_screenshot():
+    try:
+        os.rm("static/screenshot.png")
+    except:
+        pass
+    driver.save_screenshot("static/screenshot.png")
 
 @app.route("/")
 def index():
@@ -76,8 +95,9 @@ def index():
         send_whatsapp_message(data)
         return render_template("index.html")
     except:
-        error = "missing argument (name, number, or message)"
-        return render_template("error.html", error=error)
+        error = "missing argument (name, number, or message), check if QR scan scene is present"
+        save_screenshot()
+        return render_template("error.html", error=error, time=time)
 
 if __name__ == "__main__":
     app.run()
